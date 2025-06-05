a84370e284eb7013d9e146ff79704e72217729b5
Ros-spec <rjge022604@gmail.com>
2025-06-04 21:09:18 -0500

    Update firebasetest.php

--- JWT/firebasetest.php
+++ JWT/firebasetest.php
@@ -4,11 +4,11 @@ require "config/index.php";
 
 $deviceToken = $_POST["token"];
 
-# enviarCorreo("dfraga547@gmail.com", "Firebase Ventas App Log-" . date("Y-m-d"), date("H:i:s") . "<hr><p><b>Token:</b> $deviceToken</p>");
-# $fo = fopen("log.txt", "w");
-# fwrite($fo, $deviceToken);
-# fclose($fo);
-# exit;
+// enviarCorreo("dfraga547@gmail.com", "Firebase Ventas App Log-" . date("Y-m-d"), date("H:i:s") . "<hr><p><b>Token:</b> $deviceToken</p>");
+$fo = fopen("log.txt", "w");
+fwrite($fo, $deviceToken);
+fclose($fo);
+exit;
 
 $message = array(
     "token" => $deviceToken,
